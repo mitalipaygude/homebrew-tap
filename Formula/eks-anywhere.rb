ef9d1e8fa04c4362cc69b7addcc52c640f18b796
Mitali Paygude <mpaygude@amazon.com>
2022-04-19 01:42:08 -0700

    test

--- Formula/eks-anywhere.rb
+++ Formula/eks-anywhere.rb
@@ -1,16 +1,16 @@
 class EksAnywhere < Formula
   desc "CLI for managing EKS Anywhere Kubernetes clusters"
   homepage "https://github.com/aws/eks-anywhere"
-  version "0.8.2"
+  version "0.8.0"
 
   if OS.mac?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/10/artifacts/eks-a/v0.8.2/darwin/amd64/eksctl-anywhere-v0.8.2-darwin-amd64.tar.gz"
-    sha256 "46c58a364f90ebda4e8e89739468cafad26c5f34d02af93339bd6957a021d547"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/8/artifacts/eks-a/v0.8.0/darwin/amd64/eksctl-anywhere-v0.8.2-darwin-amd64.tar.gz"
+    sha256 "46c58a364f90ebda4e8e89739468cafad26c5f34d02af93339bd6957a021d54"
   end
 
   if OS.linux?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/10/artifacts/eks-a/v0.8.2/linux/amd64/eksctl-anywhere-v0.8.2-linux-amd64.tar.gz"
-    sha256 "81378b7adfc3b71091464688b3c09afc25e279f6439ca8c025795c5469e61793"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/8/artifacts/eks-a/v0.8.0/linux/amd64/eksctl-anywhere-v0.8.2-linux-amd64.tar.gz"
+    sha256 "81378b7adfc3b71091464688b3c09afc25e279f6439ca8c025795c5469e6179"
   end
 
   if Hardware::CPU.arm?
