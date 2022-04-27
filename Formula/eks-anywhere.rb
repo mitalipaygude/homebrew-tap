eb5afae56f6e1a82db444b696c0edcb68c74065e
Mitali Paygude <mpaygude@amazon.com>
2022-04-27 16:53:06 -0700

    Update eks-anywhere.rb

--- Formula/eks-anywhere.rb
+++ Formula/eks-anywhere.rb
@@ -4,12 +4,12 @@ class EksAnywhere < Formula
   version "0.8.0"
 
   if OS.mac?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/8/artifacts/eks-a/v0.8.0/darwin/amd64/eksctl-anywhere-v0.8.2-darwin-amd64.tar.gz"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/8/artifacts/eks-a/v0.8.0/darwin/amd64/eksctl-anywhere-v0.8.0-darwin-amd64.tar.gz"
     sha256 "46c58a364f90ebda4e8e89739468cafad26c5f34d02af93339bd6957a021d54"
   end
 
   if OS.linux?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/8/artifacts/eks-a/v0.8.0/linux/amd64/eksctl-anywhere-v0.8.2-linux-amd64.tar.gz"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/8/artifacts/eks-a/v0.8.0/linux/amd64/eksctl-anywhere-v0.8.0-linux-amd64.tar.gz"
     sha256 "81378b7adfc3b71091464688b3c09afc25e279f6439ca8c025795c5469e6179"
   end
 
